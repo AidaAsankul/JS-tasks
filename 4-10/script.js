88aea8c1e458be4e9ffc833ffe112f7b43c4af76
Aida Asankul <aydalmalik@gmail.com>
2022-10-11 06:57:02 +0600

    feat: 06-10

--- 4-10/script.js
+++ 4-10/script.js
@@ -6,11 +6,11 @@
    num = num - 1;
    console.log(num);*/
 
-let num = 10;
-num++;
-num++;
-num--;
-console.log(num);
+let num1 = 10;
+++num1;
+++num1;
+--num1;
+console.log(num1);
 
 /****** Переделайте этот код так, чтобы в нем использовались операторы присвоения:*********/
 /*
@@ -21,12 +21,12 @@ num = num * 10;
 num = num / 15;
 alert(num);*/
 
-let num1 = 47;
+/*let num1 = 47;
 num1 += 7;
 num1 -= 18;
 num1 *= 10;
 num1 /= 15;
-alert(num1);
+alert(num1);*/
 
 /***********Переделайте приведенный код так, чтобы в нем использовались операции
     +=, -=, *=, /=, ++, --.
@@ -41,6 +41,7 @@ num = num + 1;
 num = num - 1;
 alert(num);*/
 
+/*
 let num2 = 1;
 num2 += 12;
 num2 -= 14;
@@ -49,3 +50,25 @@ num2 /= 7;
 num2++;
 num2--;
 alert(num2);
+*/
+/******************************  && *****************************/
+
+const car = 'Mers' ;
+const color = 'Black';
+const result = car === 'Mers' && color === 'Black';
+console.log(result)
+
+const cat_name = 'Murka';
+const cat_age = 2;
+const result2 = cat_name === 'Ulan' && cat_age <= 4;
+console.log(result2)
+
+const gorod = 'Bishkek' ;
+const strana = 'Kyrgyzstan';
+const result3 = gorod === 'Bishkek' && strana === '100';
+console.log(result3)
+
+const menu = 'Manty' ;
+const sena = 150;
+const result4 = menu === 'Boorsok' && sena === '100';
+console.log(result4)
