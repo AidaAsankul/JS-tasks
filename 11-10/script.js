c0d0fa0b0b379be48fb007df7a236eb4d7576571
Aida Asankul <aydalmalik@gmail.com>
2022-10-13 19:17:55 +0600

    feat: 11-10

---
+++ 11-10/script.js
@@ -0,0 +1,33 @@
+/*
+Создайте объект заработных плат.
+    Назовите userSalaries.
+    Свойствами выступают: Ella, Sophia, Ellie
+Выведите на экран зарплату Ella, Sophia.*/
+
+let userSalaries = {'Ella':'10 000', 'Sophia':'12 000', 'Ellie':'15 000'};
+console.log(userSalaries.Ella,userSalaries.Sophia);
+
+
+/*
+Создайте объект с ключами a, b и c значениями 5 , 6 и 10.
+Найдите сумму его элементов.*/
+
+    let arr = {
+    a: 5,
+    b: 6,
+    c: 10
+    };
+alert( arr.a + arr.b + arr.c ); //21
+
+/*Сделайте рефакторинг кода, с помощью тернарного оператора.*/
+
+/*let number;
+if(data === true){
+    number = 3;
+} else{
+    number = 5;
+}*/
+
+console.log(number === true ? 3 : 5);
+
+
