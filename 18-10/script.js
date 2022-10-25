d582ab51cc298062139fa0589ff69b8009b9432b
Aida Asankul <aydalmalik@gmail.com>
2022-10-25 19:30:22 +0600

    feat:20-10

--- 18-10/script.js
+++ 18-10/script.js
@@ -5,11 +5,11 @@
 // let result = hello();
 // console.log(result);
 
-const hello = () => {
-    let message = "Привет, JavaScript!";
-    return message;
-}
-alert(hello());
+// const hello = () => {
+//     let message = "Привет, JavaScript!";
+//     return message;
+// }
+// alert(hello());
 
 
 /*************** HW 18-10**************/
@@ -18,12 +18,12 @@ alert(hello());
 и затем ее возвращает. Функция должна первым параметром принимать
 элемент (значение для массива), вторым параметром размер массива.
     Например: myFunc('a', 3)   // ['a', 'a', 'a']*/
-
-const fillArr = (value, arraySize) => {
-    return new Array(arraySize).fill(value);
-};
-
-console.log(fillArr("a",3 ));
+//
+// const fillArr = (value, arraySize) => {
+//     return new Array(arraySize).fill(value);
+// };
+//
+// console.log(fillArr("a",3 ));
 
 /*
 Напишите функцию, которая разворачивает массив в обратном порядке
@@ -32,9 +32,9 @@ console.log(fillArr("a",3 ));
 const data = [1, 2, 3];
 myFunc(data);  // [3, 2, 1];*/
 
-const myFunc = array => array.map((item, index, listing) => listing[listing.length - 1 - index]);
-const data = [1, 2, 3];
-console.log(myFunc(data));
+// const myFunc = array => array.map((item, index, listing) => listing[listing.length - 1 - index]);
+// const data = [1, 2, 3];
+// console.log(myFunc(data));
 
 /*Составьте программу, которая принимает с клавиатуры числа, пока не будет
 введено значение 0.
@@ -43,3 +43,15 @@ console.log(myFunc(data));
     Например: ввод: 7, 14, -18, 0 ⇒ вывод: плюс, плюс, минус*/
 
 
+const sortValues = () => {
+    let number = +prompt('Введите число');
+
+    if (number < 0){
+        alert("минус");
+        sortValues();
+    }else if (number > 0 && number !==0){
+        alert("плюс");
+        sortValues();
+    }
+}
+sortValues();
